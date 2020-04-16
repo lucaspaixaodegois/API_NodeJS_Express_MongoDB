da4797ff1e445f53473c092af42f85a66d0e2d7e
Lucas Paixão <lucaspaixaodegois@gmail.com>
2020-04-16 15:10:38 -0300

    add comentarios

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -1,13 +1,12 @@
-const express = require('express');
-const authMiddleware = require('../middlewares/auth');
+const express = require('express');//importando o express
+const authMiddleware = require('../middlewares/auth');//importando o middlewares
 
-const router = express.Router();
+const router = express.Router();//importando os metodos do Router() para router
 
-router.use(authMiddleware);
+router.use(authMiddleware);//definindo o uso authMiddleware antes de carregar a rota /projects
 
-router.get('/', (request, response) => {
-  response.send({ ok: true });
-
+router.get('/', (request, response) => { //metodo get para teste
+  response.send({ ok: true });//mostrar na tela ok:true se deu tudo certo no authMiddleware
 });
 
-module.exports = (app) => app.use('/projects', router);
+module.exports = (app) => app.use('/projects', router);//exportando a rota /project
